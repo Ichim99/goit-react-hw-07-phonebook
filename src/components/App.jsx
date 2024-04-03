d37c52eb16224ae72fe9e00cab4b206bed24bfaa
Ichim99 <140713412+Ichim99@users.noreply.github.com>
2024-04-03 21:18:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { nanoid } from 'nanoid';
+//import { nanoid } from 'nanoid';
 import {ContactForm} from './ContactForm/ContactForm';
 import {ContactList} from './ContactList/ContactList';
 import {Filter} from './Filter/Filtre';
